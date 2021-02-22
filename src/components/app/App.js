62d183b802ad25882f4c36000d93d232a0ffbd25
revuwem <revuwem@gmail.com>
2021-02-23 02:05:53 +0300

    base project structure created

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Sidebar from '../sidebar';
+import View from '../view';
+
+const App = () => {
+    return (
+        <>
+            <h1>Weather App</h1>
+            <Sidebar />
+            <View />
+        </>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app";
+
+import "./styles/main.scss";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
